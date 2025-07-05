29cf12d56cb7145825eb58f2192ea9cf85460e6c
devSajjad <sjdzarepur@gmail.com>
2025-07-05 21:44:36 +0330

    feat: integrate guest user creation and session management in authentication

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,11 +1,30 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 export const { signIn, signOut, handlers, auth } = NextAuth({
   providers: [Google],
   callbacks: {
     authorized({ request, auth }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const isUserExist = await getGuest(user.email);
+
+        if (!isUserExist)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+    async session({ session }) {
+      const user = await getGuest(session.user.email);
+      session.user.guestId = user.id;
+
+      return session;
     },
   },
   pages: {
--- app/account/page.js
+++ app/account/page.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 async function app() {
   const session = await auth();
-
+  console.log(session);
   return (
     <h2 className="text-accent-400 mb-7 text-2xl font-semibold">
       Welcome {session.user.name}
